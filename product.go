8401c579e02e7f713d2a9e47746d980d4b1fcc14
kacicinho <kallaoua@icloud.com>
2023-05-02 15:52:40 +0200

    exercise #01 that do product numbers in array

---
+++ product.go
@@ -0,0 +1,30 @@
+/*
+
+There is a function signFunc(x) that returns:
+
+1 if x is positive.
+-1 if x is negative.
+0 if x is equal to 0.
+You are given an integer array nums. Let product be the product of all values in the array nums.
+
+Return signFunc(product).
+
+*/
+
+func arraySign(nums []int) int {
+
+    product := 1
+
+    for i:=0;i<len(nums);i++ {
+        product *= nums[i]
+    }
+
+    if (product > 0){
+        return 1
+    } else if (product < 0){
+        return -1
+    } else {
+        return 0
+    }
+}
+
