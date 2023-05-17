0f4326f9dd40220c7fcd04600e7df0c1586d9ba1
kacicinho <kallaoua@icloud.com>
2023-05-17 15:18:48 +0200

    remove duplicates

---
+++ remove_duplicates.go
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+ func deleteDuplicates(head *ListNode) *ListNode {
+    current := head
+    for current!= nil {
+        if (current.Next != nil  && current.Val == current.Next.Val){
+            current.Next = current.Next.Next
+        } else {
+            current = current.Next
+        }
+    }
+    return head
+}
